3efe38bf7af707e30c90aef0c95767be0ea9d97b
LavenderMorara <lavender.morara@student.moringaschool.com>
2024-07-25 10:57:18 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    let form=document.querySelector('#shoppinhForm');
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        makeShoppingList(e.target.shoppingItem.value)
+        form.reset()
+    })
+})
+function makeShoppingList(shoppingItem){
+    let p =document.createElement('p');
+    let btn =document.createElement('button');
+    btn.addEventListener ('click',clearItem);
+    btn.textContent='clear';
+    p.textContent=`${shoppingItem}`;
+    p.appendChild(btn);
+    document.querySelector('#shoppingList_container').appendChild(p);
+}
+function clearItem(e){
+    e.targepat.parentNode.remove();
+}
